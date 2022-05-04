8be99575455a488f844d1842decf24c81acf0462
smckelli <smckelli@gmail.com>
2022-05-04 18:35:34 -0500

    worked the tests for the Employee.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,10 +5,27 @@ const employee = new Employee();
 describe('Employee', () => {
     describe('name', () => {
         test('Gets employee name', () => {
-            // Arrange
+            
             const name = 'Dave'
 
-            expect(employee.name).toBe(name)
+            expect(employee.name).toBe(name);
         })
     })
+    describe('id', () => {
+        test('Gets employee id', () => {
+
+            const num = 10;
+
+            expect(employee.id).toBe(num);
+        })
+    })
+    describe('email', () => {
+        test('Gets employee email', () => {
+
+            const email = "fake@email.com";
+
+            expect(employee.email).toBe(email);
+        })
+    })
+    
 })
